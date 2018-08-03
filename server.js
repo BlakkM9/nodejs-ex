ec84e7b113eb0e402222202687a5bd66540acb6e
BuildTools <unconfigured@null.spigotmc.org>
2018-08-03 22:30:20 +0200

    Added MySQL dependecy Try to connect to mysql db

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 //  OpenShift sample Node application
 var express	= require("express");
 var app		= express();
-var morgan = require("morgan");
+// var morgan = require("morgan");
 var mysql = require("mysql");
     
-Object.assign = require('object-assign');
+// Object.assign = require('object-assign');
 
 app.engine('html', require('ejs').renderFile);
 // app.set("views", __dirname + "/views");
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 app.get("/", function(req, res) {
 	console.log("redirected");
 	res.send("TEST THE CLICKS");
 });
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-    // mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    // mongoURLLabel = "";
+var port = 8080;
+var ip   = "0.0.0.0";
 
-const con = sql.createConnection({
+const con = mysql.createConnection({
     host: "db4free.net",
 	port: "3306",
     user: "access",
